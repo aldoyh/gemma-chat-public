c6ffe695047c1b7e15dc78cd81cfeef280a4890f
Hasan AlDoy <aldoyh@gmail.com>
2026-05-16 11:08:24 +0300

    types: ensure window.electron and window.api typing exists

--- src/renderer/src/env.d.ts
+++ src/renderer/src/env.d.ts
@@ -5,6 +5,14 @@ import type { Api } from '../../preload'
 declare global {
   interface Window {
     api: Api
+    // Alternative IPC interface for Electron conventions
+    electron?: {
+      ipcRenderer: {
+        invoke: (channel: string, ...args: any[]) => Promise<any>
+        on: (channel: string, listener: (...args: any[]) => void) => void
+        off: (channel: string, listener: (...args: any[]) => void) => void
+      }
+    }
   }
 }
 
